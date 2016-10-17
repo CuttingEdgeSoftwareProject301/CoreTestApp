d5ed21e200585fc8c0c5ce586a1b6cbb6d91c531
David <David@DAVID>
2016-10-17 20:31:31 +0100

    Add project files.

---
+++ src/CoreTestApp/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTestApp.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
