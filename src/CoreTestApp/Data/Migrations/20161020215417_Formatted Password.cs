7129409a3391b69156e403d72bba3090eaffb3ad
PaulLittle <S00156762@mail.itsligo.ie>
2016-10-20 22:57:07 +0100

    Formatted Password

---
+++ src/CoreTestApp/Data/Migrations/20161020215417_Formatted Password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreTestApp.Data.Migrations
+{
+    public partial class FormattedPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
