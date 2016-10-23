1ccd284a0fe139a50f5db1a75d4af44b17939a07
clairesligo <clairesligo@hotmail.com>
2016-10-23 10:35:03 +0100

    Registration page update

--- src/CoreTestApp/Models/AccountViewModels/RegisterViewModel.cs
+++ src/CoreTestApp/Models/AccountViewModels/RegisterViewModel.cs
@@ -23,5 +23,44 @@ namespace CoreTestApp.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Surame")]
+        public string Surname { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "County")]
+        public string County { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Nationality")]
+        public string Nationality { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Of Birth")]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
     }
 }
